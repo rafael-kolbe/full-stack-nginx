cf53f6f2aed40962b8a4dfa846322ae24fe70681
Rafael Kolbe <rafa.kolbe@hotmail.com>
2023-10-02 09:03:01 -0300

    Modificado para 3 instancias balanceadas de express

--- express-api/src/routes.js
+++ express-api/src/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/v1/", (_, res) => {
-    res.json({ message: "Hello express!" });
+    res.json({ message: `Hello express! From PORT: ${process.env.PORT}` });
 });
 
 module.exports = {
